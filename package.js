c2d6bfa384e6a16b58fdcd9f3bba4d3f835f389d
Michael Bishop <michael@michaelsplace.net>
2014-04-25 16:32:51 -0700

    update jsPDF

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
 Package.on_use(function (api){
 	api.add_files('libs/png_support/zlib.js', 'client');
 	api.add_files('libs/png_support/png.js', 'client');
-	api.add_files('dist/jspdf.source.js', 'client');
+	api.add_files('dist/jspdf.debug.js', 'client');
 	api.add_files('export-jspdf.js', 'client');
 	api.add_files('jspdf.plugin.png_support.js', 'client');
 
